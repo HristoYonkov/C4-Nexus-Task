fea68474b6d78d4bd55ff2d77ff56c564fd30aff
Hristo Yonkov <hristopturs@gmail.com>
2023-07-27 21:00:41 +0300

    Add App.js

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.scss';
+
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className='app__main'>
+      
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
