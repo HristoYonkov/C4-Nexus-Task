07f75a3713848819d83f974fa6aa02c1a7653279
Hristo Yonkov <hristopturs@gmail.com>
2023-07-28 21:40:32 +0300

    created filter placeholder

---
+++ client/src/components/Filter/Filter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Filter.scss'
+
+const Filter = () => {
+    
+  return (
+    <div className='app__filter'>
+        FILTER
+    </div>
+  )
+}
+
+export default Filter
