6ecfb5cac5277a7525c10520c05a48a1b30fb645
Hristo Yonkov <hristopturs@gmail.com>
2023-07-28 17:51:03 +0300

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import './App.scss';
+import { Route, Routes } from "react-router-dom";
 
 import Footer from './container/Footer/Footer';
 import Header from './container/Header/Header';
+import Card from './components/Card/Card';
 
 function App() {
+
   return (
     <div className="app">
       <Header />
       <main className='app__main'>
-        
+        <Routes>
+          <Route path='/' element={<Card />} />
+          <Route path='/laptops' element={<Card />} />
+          <Route path='/monitors' element={<Card />} />
+          <Route path='/mouses' element={<Card />} />
+          <Route path='/keyboards' element={<Card />} />
+        </Routes>
       </main>
       <Footer />
     </div>
