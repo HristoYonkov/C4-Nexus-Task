06aa1475c41e0eb81c8418e4fda46c5360f5601c
Hristo Yonkov <hristopturs@gmail.com>
2023-07-31 20:55:39 +0300

    Update FilterMobile.js

--- client/src/components/FilterMobile/FilterMobile.js
+++ client/src/components/FilterMobile/FilterMobile.js
@@ -5,17 +5,25 @@ import { motion } from 'framer-motion';
 
 import './FilterMobile.scss'
 
-const FilterMobile = () => {
+const FilterMobile = ({ changeState, originalState, minMaxPrice }) => {
     const [toggle, setToggle] = useState(false);
-
-    const [filter, setFilter] = useState({
-        priceRangeMobile: 1
+    const [selected, setSelected] = useState([]);
+    const [filteredPriceMobile, setFilteredPriceMobile] = useState({
+        priceRangeMobile: minMaxPrice.min
     });
 
-    const handleOnchange = (e) => {
+    const handleChangePrice = (e) => {
         const value = e.target.value
         const name = e.target.name
-        setFilter(prev => ({ ...prev, [name]: value }));
+        setFilteredPriceMobile(prev => ({ ...prev, [name]: value }));
+    }
+
+    const handleChangeColors = (e) => {
+        if (e.target.checked) {
+            changeState((state) => state.filter((x) => x.color !== 'white'));
+        } else {
+            changeState(originalState);
+        }
     }
 
     return (
@@ -35,28 +43,27 @@ const FilterMobile = () => {
                             <div className='app__filterMobile-colors'>
                                 <h4>Colors</h4>
                                 <div>
-                                    <input id='whiteMobile' type="checkbox" />
+                                    <input id='whiteMobile'  value="white" type="checkbox" onChange={handleChangeColors} />
                                     <label htmlFor='whiteMobile'>White</label>
                                 </div>
 
                                 <div>
-                                    <input id='blackMobile' type="checkbox" />
+                                    <input id='blackMobile'  value="black" type="checkbox" onChange={handleChangeColors} />
                                     <label htmlFor='blackMobile'>Black</label>
                                 </div>
 
                                 <div>
-                                    <input id='blueMobile' type="checkbox" />
+                                    <input id='blueMobile'  value="blue" type="checkbox" onChange={handleChangeColors} />
                                     <label htmlFor='blueMobile'>Blue</label>
                                 </div>
                             </div>
 
                             <div className='app__filterMobile-price'>
                                 <h4>Price</h4>
-                                <input name='priceRangeMobile' value={filter.priceRangeMobile} id='rangeValueMobile' onChange={handleOnchange} type="range" min="1" max="200" />
-                                <label id="rangeValueMobile"><p>${filter.priceRangeMobile} - $200</p></label>
+                                <input name='priceRangeMobile' value={filteredPriceMobile.priceRangeMobile} id='priceRangeMobile' onInput={handleChangePrice} type="range" min={minMaxPrice.min} max={minMaxPrice.max} />
+                                <label id="priceRangeMobile"><p>${filteredPriceMobile.priceRangeMobile} - ${minMaxPrice.max}</p></label>
                             </div>
                         </div>
-
                     </motion.section>
                 )}
             </div>
