08b9bd8149f7cabd64dc625bafff91a605b5759a
Hristo Yonkov <hristopturs@gmail.com>
2023-07-31 22:30:04 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
             <Laptops
               state={laptopsState}
               originalState={backupLaptops}
-              changeState={setLaptopsState}
+              currentState={setLaptopsState}
             />}
           />
           
