afbf8fb3e1cc4ba1fd79e7ed5d53417dbf3cd090
Hristo Yonkov <hristopturs@gmail.com>
2023-07-31 20:55:37 +0300

    Update Filter.js

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Filter.scss';
 
-const Filter = () => {
+const Filter = ({ changeState, originalState, minMaxPrice }) => {
+  const [selected, setSelected] = useState([]);
   const [filteredPrice, setFilteredPrice] = useState({
-    price: 1
+    price: minMaxPrice.min
   });
 
-  const handleOnchange = (e) => {
-    console.log(e.target);
+  const handleChangePrice = (e) => {
     const value = e.target.value
     const name = e.target.name
     setFilteredPrice(prev => ({ ...prev, [name]: value }));
+  }
+
+  const handleChangeColors = (e) => {
+    if (e.target.checked) {
+      changeState((state) => state.filter((x) => x.color !== 'white'));
+    } else {
+      changeState(state => originalState);
+    }
   }
 
   return (
@@ -18,25 +26,25 @@ const Filter = () => {
       <div className='app__filter-colors'>
         <h4>Colors</h4>
         <div>
-        <input id='white' type="checkbox" />
+          <input id='white' value="white" type="checkbox" onChange={handleChangeColors} />
           <label htmlFor='white'>White</label>
         </div>
 
         <div>
-          <input id='black' type="checkbox" />
+          <input id='black' type="checkbox" value="black" onChange={handleChangeColors} />
           <label htmlFor='black'>Black</label>
         </div>
 
         <div>
-        <input id='blue' type="checkbox" />
+          <input id='blue' type="checkbox" value="blue" onChange={handleChangeColors} />
           <label htmlFor='blue'>Blue</label>
         </div>
       </div>
 
       <div className='app__filter-price'>
         <h4>Price</h4>
-        <input name='price' value={filteredPrice.price} id='price' onChange={handleOnchange} type="range" min="1" max="200" />
-        <label id="price"><p>${filteredPrice.price} - $200</p></label>
+        <input name='price' value={filteredPrice.price} id='price' onInput={handleChangePrice} type="range" min={minMaxPrice.min} max={minMaxPrice.max} />
+        <label id="price"><p>${filteredPrice.price} - ${minMaxPrice.max}</p></label>
       </div>
     </section>
   )
