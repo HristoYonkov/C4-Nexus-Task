399f34f8cff8c6574fa9c35af7516b177e0012be
Hristo Yonkov <hristopturs@gmail.com>
2023-07-31 12:53:24 +0300

    add calculate discount functionality to the card component

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -3,27 +3,40 @@ import { FaRegStar, FaStar } from 'react-icons/fa'
 
 import './Card.scss'
 
-const Card = () => {
+const Card = (props) => {
+    const item = props.item;
+
+    const calculateDiscount = (disc) => {
+        const discount = item.price - (item.price / 100 * Number(disc));
+        return discount;
+    }
+
 
     return (
         <div id="container">
             <div className="container__card">
-                <img src="https://www.asus.com/media/Odin/Websites/global/Series/12.png" alt="Lago di Braies" />
+                <img src={item.imageUrl} alt="Lago di Braies" />
 
                 <div className="card__details">
-                    <span className="card__details-tag">Laptop</span>
+                    <span className="card__details-tag">{item.category}</span>
 
                     <div className='card__details-pricing'>
-                        <span className="card__details-discount">-30%</span>
-                        <span className="card__details-price" style={{ textDecoration: 'line-through' }}>$354.00</span>
-                        <span className="card__details-price">$254.00</span>
+                        {item.discount > 0 && (
+                            <>
+                                <span className="card__details-discount">{item.discount}%</span>
+                                <span className="card__details-price" style={{ textDecoration: 'line-through' }}>${item.price}</span>
+                            </>
+                        )}
+                        
+                        <span className="card__details-price">${calculateDiscount(item.discount)}</span>
                     </div>
 
-                    <div className="card__details-name">Asus Vivobook</div>
+                    <div className="card__details-name">{item.name}</div>
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
                     <div className='card__details-stars'>
+                        {}
                         <FaStar />
                         <FaStar />
                         <FaStar />
@@ -34,7 +47,7 @@ const Card = () => {
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
 
