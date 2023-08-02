c8947d2efc3ed9684e557d8f0355cf32ee8077ae
Hristo Yonkov <hristopturs@gmail.com>
2023-08-02 21:32:02 +0300

    Delete context.js

--- client/src/context/context.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import laptopsData from './data/laptopsData';
-
-const [laptopsState, setLaptopsState] = useState(laptopsData);
-export const AuthContext = createContext();
